77b236c88e95d2d786a727774928b0402a88acd8
Hooseun <rimzeljhn@gmail.com>
2024-05-30 21:36:28 +0800

    completed Events and Muni_abouts

--- src/events/events.controller.js
+++ src/events/events.controller.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const Events = require("./events.model");
 const BrgyInfo = require("../brgy_info/brgy_info.model");
-const compareArrays = require("../../global/functions/CompareArrays");
+const CompareArrays = require("../../global/functions/CompareArrays");
 const GenerateID = require("../../global/functions/GenerateID");
 const { UploadFiles, CreateFolder, DeleteFiles } = require("../../global/utils/Drive");
-const eventsModel = require("./events.model");
+
 
 const GetBarangayEvents = async (req, res) => {
   try {
-    const { brgy, archived } = req.query;
+    const { brgy, isArchived } = req.query;
     let query = {
       brgy: brgy,
-      ...(archived !== undefined && { isArchived: archived }),
+      ...(isArchived !== undefined && { isArchived: isArchived }),
     };
 
     const result = await Events.find(query)
@@ -27,22 +27,24 @@ const GetBarangayEvents = async (req, res) => {
   }
 };
 
-const SearchBarangayEvents = async (req, res) => {
+const GetAllOpenBrgyEvents = async (req, res) => {
   try {
-    const { brgy, archived } = req.query;
+    const { brgy } = req.query;
 
-    let query = {
-      brgy: brgy,
-      ...(archived !== undefined && { isArchived: archived }),
+    const query = {
+      $and: [
+        { isArchived: false },
+        { $or: [{ brgy: brgy }, { isOpen: true }] },
+      ],
     };
 
-    const result = await Events.find(query)
+    const result = await Events.find(query).sort({ createdAt: -1 });
 
-    return !result
-      ? res
-        .status(400)
-        .json({ error: `No such Events for Barangay ${brgy}` })
-      : res.status(200).json({ result });
+    return res.status(200).json({
+      result,
+      pageCount: Math.ceil(result.length / 10),
+      total: result.length, // Total count without pagination
+    });
   } catch (err) {
     res.send(err.message);
   }
@@ -52,14 +54,14 @@ const CreateEvents = async (req, res) => {
   try {
     const { event_folder_id } = req.query;
     const { body, files } = req;
-    const { title, details, date, end_reg_date, brgy, isOpen } = JSON.parse(body.events); // Added end_reg_date here
+    const { event_name, details, date, end_reg_date, brgy, isOpen } = JSON.parse(body.events); // Added end_reg_date here
 
     let fileArray = [];
     const increment = await Events.countDocuments({}).exec();
     const brgys = await BrgyInfo.find({}).sort({ createdAt: 1 }).exec();
 
     const index = brgys.findIndex((item) => item.brgy === brgy) + 1;
-    const event_id = GenerateID(index, "events", increment);
+    const event_id = GenerateID(index + 1, "events", increment + 1);
     const folder_id = await CreateFolder(event_id, event_folder_id);
 
     for (let f = 0; f < files.length; f += 1) {
@@ -81,7 +83,7 @@ const CreateEvents = async (req, res) => {
 
     const result = await Events.create({
       event_id,
-      title,
+      event_name,
       details,
       event_date: date, 
       end_reg_date, 
@@ -102,5 +104,156 @@ const CreateEvents = async (req, res) => {
   }
 };
 
+const UpdateEvent = async (req, res) => {
+  try {
+    const { id } = req.query;
+    let { body, files } = req;
+    let currentFiles = [];
+    body = JSON.parse(JSON.stringify(req.body));
+    let { saved, event } = body;
+    let banner = null,
+      logo = null;
 
-module.exports = { GetBarangayEvents, CreateEvents, SearchBarangayEvents };
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "No event" });
+    }
+
+    // Check if the currentFiles uploaded in this function backend is not empty
+    // Meaning, there are files in the frontend
+    if (saved !== undefined) {
+      if (typeof body.saved === "object") {
+        for (const item of saved) {
+          const parsed = JSON.parse(item);
+          currentFiles.push(parsed);
+        }
+      } else {
+        const parsed = JSON.parse(saved);
+        currentFiles.push(parsed);
+      }
+    }
+
+    let fileArray = [...currentFiles];
+    event = JSON.parse(body.event);
+    const folder_id = event.collections.folder_id;
+    const fullItem = event.collections.file;
+    const toBeDeletedItems = CompareArrays(fullItem, currentFiles);
+
+    toBeDeletedItems.forEach(async (item) => {
+      await DeleteFiles(item.id, folder_id);
+    });
+
+    if (files) {
+      for (let f = 0; f < files.length; f += 1) {
+        const { id, name } = await UploadFiles(files[f], folder_id);
+
+        if (files[f].originalname === "banner") {
+          banner = {
+            link: `https://drive.google.com/thumbnail?id=${id}&sz=w1000`,
+            id,
+            name,
+          };
+
+          await DeleteFiles(
+            event.collections.banner.id,
+            folder_id
+          );
+        } else if (files[f].originalname === "logo") {
+          logo = {
+            link: `https://drive.google.com/thumbnail?id=${id}&sz=w1000`,
+            id,
+            name,
+          };
+
+          await DeleteFiles(event.collections.logo.id, folder_id);
+        } else {
+          fileArray.push({
+            link: `https://drive.google.com/file/d/${id}/view`,
+            id,
+            name,
+          });
+        }
+      }
+    }
+
+    const result = await Events.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          event_name: event.event_name,
+          details: event.details,
+          date: event.date,
+          collections: {
+            folder_id,
+            banner: banner === null ? event.collections.banner : banner,
+            logo: logo === null ? event.collections.logo : logo,
+            file: fileArray,
+          },
+          brgy: event.brgy,
+          isOpen: event.isOpen,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+const ArchiveEvents = async (req, res) => {
+  try {
+    const { id, isArchived } = req.query;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "No such Events" });
+    }
+
+    const result = await Events.findOneAndUpdate(
+      { _id: id },
+      { $set: { isArchived: isArchived } },
+      { returnOriginal: false, upsert: true }
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+const GetSpecificBarangayEvent = async (req, res) => {
+  try {
+    const { brgy, isArchived, event_id } = req.query;
+
+    const result = await Events.findOne({
+      $and: [{ brgy: brgy }, { isArchived: isArchived }, { event_id: event_id }],
+    }).sort({ createdAt: -1 });
+
+    return !result
+      ? res
+        .status(400)
+        .json({ error: `No such Events for Barangay ${brgy} and Event ID ${event_id}` })
+      : res.status(200).json({ result });
+  } catch (err) {
+    res.send(err.message);
+  }
+};
+
+const getAllEvents = async (req, res) => {
+  const { brgy } = req.query;
+
+  const result = await Events.aggregate([
+    // Unwind the "collections" array to access individual documents within it
+    { $unwind: "$collections" },
+    // Group by service name
+    { $match: { isArchived: false, $or: [{ brgy: brgy }, { isOpen: true }] } },  // Replace "ROSARIO" with your desired Barangay
+    // Group by service name and use $addToSet to get distinct values
+    { $group: { _id: "$title" } },
+    // Project only the "_id" field (which holds distinct service names)
+    { $project: { _id: 1 } }
+  ])
+
+  res.status(200).json(result)
+}
+
+module.exports = { GetBarangayEvents, CreateEvents, GetAllOpenBrgyEvents, UpdateEvent, ArchiveEvents, GetSpecificBarangayEvent, getAllEvents};
--- src/events/events.route.js
+++ src/events/events.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const upload = require("../../global/config/Multer");
 
-const { GetBarangayEvents, CreateEvents, SearchBarangayEvents } = require("./events.controller");
+const { GetBarangayEvents, CreateEvents, GetAllOpenBrgyEvents, UpdateEvent, ArchiveEvents, UpdateAttendees, GetSpecificBarangayEvent, getAllEvents } = require("./events.controller");
 
 /*
     GetEventsServices
@@ -12,7 +12,50 @@ const { GetBarangayEvents, CreateEvents, SearchBarangayEvents } = require("./eve
     @isArchived = query
 */
 router.get("/", GetBarangayEvents);
-router.get("/search", SearchBarangayEvents)
+/*
+    GetOpenBarangayEvents
+    Description: Get All Open Events in barangay
+
+    @isArchived = query
+    @isOpen = query
+*/
+router.get("/all", GetAllOpenBrgyEvents);
+/*
+    Get Distinct event
+    Description: Get Events for filter
+
+    @brgy = query aggregate of collections and title
+*/
+router.get("/get_distinct_events", getAllEvents);
+/*
+    Get Distinct event
+    Description: Get Events for filter
+
+    @brgy = query aggregate of collections and title
+*/
+router.get("/specific", GetSpecificBarangayEvent);
+/*
+    Adding Events
+    Description: Adding Events
+
+    @event_folder_id = query
+    @event_name, details, date, end_reg_date, brgy, isOpen = JSON parse
+*/
 router.post("/", upload.array("files", 10), CreateEvents);
+/*
+    UpdateEvent
+    Description: Update Event
+
+    @id = query
+    @body = JSON PARSE
+*/
+router.patch("/update", upload.array("files", 10), UpdateEvent);
+/*
+    Archive Event
+    Description: Archiving Events
+
+    @id , isArchived = Query
+*/
+router.patch("/archive", ArchiveEvents);
 
 module.exports = router;
