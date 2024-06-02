6a9478676f7588e568cce0daffd84c0bfe2f562e
Rogu <116495454+rgrgogu@users.noreply.github.com>
2024-06-02 21:08:14 +0800

    adjusted

--- src/layout_doc/layout_doc.route.js
+++ src/layout_doc/layout_doc.route.js
@@ -1,1 +1,16 @@
-console.log('hi')
+const express = require("express");
+const router = express.Router();
+
+const RequireAuth = require("../../global/middleware/RequireAuth")
+const {
+    GetAllDocumentForm,
+    CreateDocumentForm,
+    UpdateDocumentForm
+} = require("./layout_doc.controller");
+
+router.use(RequireAuth)
+router.get("/", GetAllDocumentForm);
+router.post("/", CreateDocumentForm);
+router.patch("/", UpdateDocumentForm);
+
+module.exports = router;
